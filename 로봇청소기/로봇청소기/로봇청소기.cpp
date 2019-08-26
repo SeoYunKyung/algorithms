7872aab537e69446266f034ae154501235416d30
yanggaeng <carys3115@ajou.ac.kr>
2019-08-26 17:55:18 +0900

    백준 #14503 로봇청소기

---
+++ 로봇청소기/로봇청소기/로봇청소기.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+int N, M, r, c, d;
+
+int map[50][50];
+
+void clean(int x,int y) {
+	if (map[x][y] == 1)
+}
+
+int main()
+{
+	ios::sync_with_stdio();
+	cout.tie(0);
+	cin.tie(0);
+
+	cin >> N >> M >>r >> c >>d;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	clean(r,c);
+
+
+
+
+}
